b00b2d0797a58eea3bb62569132679cc14a58f04
thialfi17 <thialfi17@gmail.com>
2024-10-16 21:13:24 +0100

    Stage 1 to a working system

---
+++ README.md
@@ -0,0 +1,14 @@
+# My Personal NixOS configurations
+
+Still a heavy work in progress. Not sure yet if I would like to switch fully
+but I like the idea of a fully reproducible system that is (supposedly) trivial
+to restore.
+
+# Credits
+
+Major inspiration has been taken from
+[jnsgruk](https://github.com/jnsgruk/nixos-config) and
+[Misterio77](https://github.com/Misterio77/nix-starter-configs).
+
+A big thanks to [LibrePhoenix](https://www.youtube.com/@librephoenix) for
+your various Nix tutorials.
--- flake.nix
+++ flake.nix
@@ -2,24 +2,41 @@
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 		nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+		home-manager = {
+			url = "github:nix-community/home-manager/release-24.05";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
 
 	outputs = {
 		self,
 		nixpkgs,
+		home-manager,
 		...
 	} @ inputs: let 
 		inherit (self) outputs;
+		stateVersion = "24.05";
 	in {
 		overlays = import ./overlays { inherit inputs; };
 
 		nixosConfigurations = {
-			nixos = nixpkgs.lib.nixosSystem {
-				specialArgs = { inherit inputs outputs; };
+			nixos = let 
+				hostname = "nixos";
+			in nixpkgs.lib.nixosSystem {
+				specialArgs = { inherit inputs outputs stateVersion; };
 				modules = [
-					./configuration.nix
-					./hardware-configuration.nix
+					./hosts/${hostname}/configuration.nix
 				];
+			};
+		};
+
+		homeConfigurations = {
+			"josh@nixos" = home-manager.lib.homeManagerConfiguration {
+				# TODO: replace this with a ${system} variable?
+				pkgs = nixpkgs.legacyPackages.x86_64-linux;
+				extraSpecialArgs = { inherit inputs outputs stateVersion; };
+				modules = [];
 			};
 		};
 	};
